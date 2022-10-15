c4ba3ee215b582c70b6ae3f5634a5e731c73ee50
Mseyitog <melk.seyitoglu@gmail.com>
2022-10-15 14:39:28 +0300

    util files updated

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,76 +6,59 @@
 /*   By: mseyitog <mseyitog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 14:25:32 by mseyitog          #+#    #+#             */
-/*   Updated: 2022/10/09 17:24:46 by mseyitog         ###   ########.fr       */
+/*   Updated: 2022/10/15 14:08:09 by mseyitog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#ifndef NDEBUG
-#include <stdio.h>
-#endif
-
-char	*ft_strnew(size_t size)
-{
-	char	*str;
-
-	str = (char *)malloc(sizeof(char) * size + 1);
-	if (!str)
-		return (NULL);
-	return (str);
-}
 
 size_t		ft_strlen(const char *s)
 {
 	size_t		i;
 
-	i = -1;
-	while (*(s + ++i))
-		;
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
 	return (i);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char	*new_str;
-	size_t	i;
-	size_t	j;
-	size_t	s1_len;
-	size_t	s2_len;
+	unsigned int	i;
+	unsigned int	j;
+	char			*s3;
 
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
 	if (!s1 || !s2)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	new_str = ft_strnew(s1_len + s2_len);
-	if (!new_str)
+	s3 = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!s3)
 		return (NULL);
 	i = -1;
+	while (s1[++i] != 0)
+		s3[i] = s1[i];
 	j = -1;
-	while (++i < s1_len)
-		*(new_str + i) = *(s1 + i);
-	while (++j < s2_len)
-		*(new_str + i++) = *(s2 + j);
+	while (s2[++j] != 0)
+	{
+		s3[i + j] = s2[j];
+	}
+	s3[i + j] = 0;
 	free(s1);
-	return (new_str);
+	return (s3);
 }
 
-int	red_lenght(char	*red, char	del)
-{
+char    *ft_strchr(const char *str, int c){
 	int	i;
 
 	i = 0;
-	while (red[i] != del)
-		++i;
-
-	return(i + 1);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	int	i;
-
-	i = 0;
+	if (!str)
+		return (NULL);
 	while (str[i])
 	{
 		if (str[i] == c % 256)
@@ -85,76 +68,4 @@ char	*ft_strchr(const char *str, int c)
 	if (str[i] == c % 256 && c % 256 == '\0')
 		return (&((char *)str)[i]);
 	return (NULL);
-}
-
-char	*clear_red(char **red, int len)
-{
-	int			nullen;
-	char	*new;
-	int			i;
-
-	i = 0;
-	nullen = red_lenght(*red, '\0');
-	new = (char *)malloc((nullen - len) * sizeof(char));
-	while (i < nullen - len)
-	{
-		new[i] = (*red)[len + i];
-		++i;
-	}
-	new[i] = 0;
-	free(*red);
-	free(red);
-	return(new);
-}
-
-char	*get_line(char **red)
-{
-	char	*line;
-	int		len;
-	int		i;
-
-	i = 0;
-	len = red_lenght(*red, '\n');
-	line = (char *)malloc(sizeof(char) * len + 1);
-	while (i < len)
-	{
-		line[i] = (*red)[i];
-		i++;
-	}
-	line[i] = 0;
-	*red = clear_red(red, len);
-	return (line);
-}
-
-char	*get_char(int fd)
-{
-	char		*get;
-	static char *red;
-	int			sonuc;
-
-	#ifndef NDEBUG
-		printf("heyo");
-	#endif
-	sonuc = 1;
-	get = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!get)
-		return (0);
-	while (!ft_strchr(get, '\n') && sonuc)
-	{
-		sonuc = read(fd, get, BUFFER_SIZE);
-		if (sonuc == -1)
-		{
-			free(get);
-			return (NULL);
-		}
-		get[BUFFER_SIZE] = 0;
-		#ifndef NDEBUG
-			printf("%s", get);
-		#endif
-		red = ft_strjoin(red, get);
-	}
-	#ifndef NDEBUG
-			printf("bitti");
-	#endif
-	return (red);
 }
